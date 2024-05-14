382d35e2653a341b920b2ad7619644cd5cf559c5
aidaiErkinbek <aerksoz@gmail.com>
2024-05-14 14:18:50 +0600

    first commit

---
+++ BookStore/src/Components/Logo/Logo.js
@@ -0,0 +1,14 @@
+import './Logo.css';
+import logos from '../../img/'
+
+function Logo() {
+  return (
+    <>
+    <a href="index.html" class="logo">
+					<img src={logos} alt=""/>
+				</a>
+    </>
+  );
+}
+
+export default Logo;
